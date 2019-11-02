770196375663f68d89f71807b8710c3b81ecbb6e
unknown <panagiwtis.orovas@gmail.com>
2019-11-02 16:54:03 +0200

    added language dropdown

--- src/Components/Navbar/RightMenu.js
+++ src/Components/Navbar/RightMenu.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Menu } from 'antd';
+import { Menu, Select } from 'antd';
 import HowWorks from './HowWorksDrawer';
 import { Link, Route } from 'react-router-dom';
 import CardLoginStyle from '../LoginForm/LoginFormStyle';
 import CardRegisterStyle from '../RegisterForm/SignUpStyle';
+
+const { Option } = Select;
+
+function handleChange(value) {
+  console.log(`Selected ${value}`);
+}
 
 class RightMenu extends Component {
   render() {
     return (
       <div>
         <Menu mode={this.props.mode}>
+          <Select
+            defaultValue='Greek'
+            style={{ width: '130px', marginBottom: '15px', marginRight: '10px', marginLeft: '10px' }}
+            onChange={handleChange}
+          >
+            <Option value='English'>Αγγλικά(ENG)</Option>
+            <Option value='Greek'>Ελληνικά(GR)</Option>
+          </Select>
+
           <Menu.Item key='howWorks'>
             <HowWorks />
           </Menu.Item>
