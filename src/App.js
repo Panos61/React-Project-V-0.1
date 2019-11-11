f82bf034de7de65c8f01da9106d4b9e5fd1fa5fd
unknown <panagiwtis.orovas@gmail.com>
2019-11-11 22:56:46 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 //import PopBtn from './Components/OtherMainPage/PopBtn';
 import Search from './Components/OtherMainPage/Search';
-import { BackTop, Layout } from 'antd';
+import { BackTop } from 'antd';
 import Cities from './Components/OtherMainPage/Cities';
 //import ContactForm from './SubPages-Test/SubPages/Contact';
 import MainHelpPage from './SubPages-Test/MainHelpPage';
 import { Route } from 'react-router-dom';
 import FooterMain from './FooterTest';
-
-const Footer = Layout;
 
 class App extends Component {
   render() {
