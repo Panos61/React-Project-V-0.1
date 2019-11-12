83badad036c537d5bccd55fd084c625db6ce3bc2
unknown <panagiwtis.orovas@gmail.com>
2019-11-13 01:41:34 +0200

    edit

--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   PageHeader,
   Tabs,
@@ -6,32 +6,32 @@ import {
   Statistic,
   Avatar,
   Timeline
-} from "antd";
-import "antd/dist/antd.css";
+} from 'antd';
+import 'antd/dist/antd.css';
 
 const { TabPane } = Tabs;
 
 const renderContent = (column = 2) => (
-  <Descriptions size="small" column={column} style={{ marginTop: "3vh" }}>
-    <Descriptions.item label="Username"></Descriptions.item>
-    <Descriptions.item label="Email"></Descriptions.item>
-    <Descriptions.item label="Nickname"></Descriptions.item>
-    <Descriptions.item label="Όνομα"></Descriptions.item>
-    <Descriptions.item label="Επίθετο"></Descriptions.item>
+  <Descriptions size='small' column={column} style={{ marginTop: '3vh' }}>
+    <Descriptions.item label='Username'></Descriptions.item>
+    <Descriptions.item label='Email'></Descriptions.item>
+    <Descriptions.item label='Nickname'></Descriptions.item>
+    <Descriptions.item label='Όνομα'></Descriptions.item>
+    <Descriptions.item label='Επίθετο'></Descriptions.item>
   </Descriptions>
 );
 
 const extraContent = (
   <div
     style={{
-      display: "flex",
-      width: "max-content",
-      justifyContent: "flex-end"
+      display: 'flex',
+      width: 'max-content',
+      justifyContent: 'flex-end'
     }}
   >
     <Statistic
-      title="Status"
-      value="Ενεργός/ή"
+      title='Status'
+      value='Ενεργός/ή'
       style={{
         marginRight: 32
       }}
@@ -41,12 +41,12 @@ const extraContent = (
 
 const Content = ({ children, extra }) => {
   return (
-    <div className="content">
-      <div className="main">
-        <Avatar shape="square" size={64} icon="user" />
+    <div className='content'>
+      <div className='main'>
+        <Avatar shape='square' size={64} icon='user' />
         {children}
       </div>
-      <div className="extra">{extra}</div>
+      <div className='extra'>{extra}</div>
     </div>
   );
 };
@@ -56,14 +56,14 @@ class ProfileEdit extends Component {
     return (
       <div>
         <PageHeader
-          style={{ border: "1px solid rgb(235,237,240)" }}
+          style={{ border: '1px solid rgb(235,237,240)' }}
           onBack={() => window.history.back()}
-          title="Επεξεργασία Προφίλ"
-          subTitle="Επεξεργασία δεδομένων"
+          title='Επεξεργασία Προφίλ'
+          subTitle='Επεξεργασία δεδομένων'
           footer={
-            <Tabs defaultActiveKey="1">
-              <TabPane tab="Ιστορικό" key="1">
-                <Timeline style={{ marginTop: "5vh" }}>
+            <Tabs defaultActiveKey='1'>
+              <TabPane tab='Ιστορικό' key='1'>
+                <Timeline style={{ marginTop: '5vh' }}>
                   <Timeline.Item>
                     Δημιουργία Λογαριασμού 12-11-2019
                   </Timeline.Item>
@@ -75,9 +75,9 @@ class ProfileEdit extends Component {
                 </Timeline>
               </TabPane>
               <TabPane
-                tab="Λοιπές Πληροφορίες"
-                key="2"
-                style={{ marginTop: "5vh" }}
+                tab='Λοιπές Πληροφορίες'
+                key='2'
+                style={{ marginTop: '5vh' }}
               >
                 ...
               </TabPane>
