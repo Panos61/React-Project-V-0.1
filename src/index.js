d26427b39fd4ef94b83f93d8e16e911e2b6e7684
unknown <panagiwtis.orovas@gmail.com>
2019-11-12 23:05:28 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 //import './index.css';
-import 'antd/dist/antd.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import CardRegisterStyle from './Components/RegisterForm/SignUpStyle';
-import CardLoginForm from './Components/LoginForm/LoginFormStyle';
+import "antd/dist/antd.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import CardRegisterStyle from "./Components/RegisterForm/SignUpStyle";
+import CardLoginForm from "./Components/LoginForm/LoginFormStyle";
 //import ContactFormPage from './SubPages-Test//SubPages/Contact';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import MainHelpPage from './SubPages-Test/MainHelpPage';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import MainHelpPage from "./SubPages-Test/MainHelpPage";
+import ProfileEdit from "./ProfileEdit";
 
 //Added basic Routing for Login/Register
 const routing = (
   <Router>
     <div>
-      <Route exact path='/' component={App} />
-      <Route path='/login' component={CardLoginForm} />
-      <Route path='/register' component={CardRegisterStyle} />
-      <Route path='/Help' component={MainHelpPage} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={CardLoginForm} />
+      <Route path="/register" component={CardRegisterStyle} />
+      <Route path="/Help" component={MainHelpPage} />
+      {/* Route path Test ProfilEdit  */}
+      <Route path="/ProfileEdit" component={ProfileEdit} />
     </div>
   </Router>
 );
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 serviceWorker.unregister();
