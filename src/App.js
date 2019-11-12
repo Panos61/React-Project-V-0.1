ea890b62f4f963ea653407c738917b164e6e79bf
unknown <panagiwtis.orovas@gmail.com>
2019-11-12 21:04:21 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 //import PopBtn from './Components/OtherMainPage/PopBtn';
-import Search from './Components/OtherMainPage/Search';
-import HorizontalSider from './Components/OtherMainPage/HorizontalSider';
-import { BackTop } from 'antd';
-import Cities from './Components/OtherMainPage/Cities';
+import Search from "./Components/OtherMainPage/Search";
+import HorizontalSider from "./Components/OtherMainPage/HorizontalSider";
+import { BackTop } from "antd";
+import Cities from "./Components/OtherMainPage/Cities";
 //import ContactForm from './SubPages-Test/SubPages/Contact';
-import MainHelpPage from './SubPages-Test/MainHelpPage';
-import { Route } from 'react-router-dom';
-import FooterMain from './FooterTest';
+import MainHelpPage from "./SubPages-Test/MainHelpPage";
+import { Route } from "react-router-dom";
+import FooterMain from "./FooterTest";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className='main-page_style'>
+        <div className="main-page_style">
           <section>
             <Navbar />
 
@@ -29,7 +29,7 @@ class App extends Component {
           <FooterMain />
         </footer>
         {/* Routes */}
-        <Route path='/Help' Component={MainHelpPage} />
+        <Route path="/Help" Component={MainHelpPage} />
       </div>
     );
   }
