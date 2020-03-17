09d54a4b6e6a89eaad4346666e1e6ea901fb24b6
Panos61 <panagiwtis.orovas@gmail.com>
2020-03-17 12:54:16 +0200

    Alerts

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,105 +1,138 @@
-import React from 'react';
-import { LockOutlined, MailOutlined } from '@ant-design/icons';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Input, Button, Checkbox, Alert } from 'antd';
-import { Link, Route, BrowserRouter } from 'react-router-dom';
-import './LoginForm.css';
-import CardRegisterStyle from '../RegisterForm/SignUpStyle';
-import PropTypes from 'prop-types';
-import App from '../../App';
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { Form, Input, Button, Checkbox, Alert } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import { MailOutlined, LockOutlined } from '@ant-design/icons';
 
 import { connect } from 'react-redux';
 import { login } from '../../actions/authActions';
 import { clearErrors } from '../../actions/errorActions';
+import PropTypes from 'prop-types';
 
-class LoginForm extends React.Component {
+const layout = {
+  labelCol: {
+    span: 8
+  },
+  wrapperCol: {
+    span: 16
+  }
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16
+  }
+};
+
+class WrappedNormalLoginForm extends Component {
   static propTypes = {
-    isAutheticated: PropTypes.bool,
+    isAuthenticated: PropTypes.bool,
     login: PropTypes.func.isRequired,
-    error: PropTypes.object.isRequired
+    error: PropTypes.object.isRequired,
+    clearErrors: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields(['email', 'password'], (err, values) => {
-      this.props.login(values);
-      console.warn(err);
-    });
+  state = {
+    message: null
   };
+
+  componentDidUpdate(prevProps) {
+    const { auth } = this.props;
+    if (auth !== prevProps.auth) {
+      // Check for login error
+      if (auth.status === false) {
+        this.setState({ message: auth.message });
+      } else {
+        this.setState({ message: null });
+      }
+    }
+  }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div>
-        {/*Alert Component */}
-        <div id="alertLogin" style={{ display: 'none' }}>
-          <Alert
-            showIcon={false}
-            message="Αδυναμία Σύνδεσης"
-            description="Το e-mail ή ο κωδικός που εισάγατε είναι λάθος!"
-            type="error"
-          />
-        </div>
-        {/** */}
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('email', {
-              rules: [{ required: true, message: 'Εισάγετε το Email σας!' }]
-            })(
-              <Input
-                prefix={<MailOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="E-mail"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Εισάγετε τον κωδικό σας!' }]
-            })(
-              <Input
-                prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Κωδικός"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: false
-            })(<Checkbox>Αποθήκευση</Checkbox>)}
-            <Link to="/" className="login-form-forgot">
-              Ξεχάσατε τον κωδικό;
-            </Link>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              Σύνδεση
-            </Button>
-            Ή <Link to="/register">εγγραφείτε τώρα!</Link>
-          </Form.Item>
-        </Form>
+    const onFinish = values => {
+      this.props.login(values);
+      console.log('Success:', values);
+    };
 
-        {/**ROUTES */}
-        <BrowserRouter>
-          <Route path="/" Component={App} />
-          <Route path="/register" Component={CardRegisterStyle} />
-        </BrowserRouter>
-      </div>
+    const onFinishFailed = errorInfo => {
+      console.warn('Failed:', errorInfo);
+    };
+
+    return (
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: false
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        {this.state.message ? (
+          <Alert
+            style={{ marginBottom: '5px' }}
+            type="error"
+            message="Σφάλμα Σύνδεσης"
+            description={this.state.message}
+            showIcon
+          />
+        ) : null}
+        <Form.Item
+          label="E-mail"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Εισάγετε το E-mail σας!'
+            }
+          ]}
+        >
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            type="email"
+            placeholder="E-mail"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Κωδικός"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Εισάγετε τον κωδικό σας!'
+            }
+          ]}
+        >
+          <Input.Password
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Κωδικός"
+          />
+        </Form.Item>
+        <Form.Item {...tailLayout} valuePropName="checked">
+          <Checkbox>Αποθήκευση</Checkbox>
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Σύνδεση
+          </Button>
+        </Form.Item>
+        <Form.Item style={{ marginLeft: '15px' }}>
+          Ή <Link to="/register">εγγραφείτε τώρα!</Link>
+        </Form.Item>
+      </Form>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  isAutheticated: state.auth.isAutheticated,
-  error: state.error
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+  auth: state.auth
 });
 
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(LoginForm);
-
-export default connect(mapStateToProps, { login, clearErrors })(
-  WrappedNormalLoginForm
-);
+export default compose(
+  withRouter,
+  connect(mapStateToProps, { login, clearErrors })
+)(WrappedNormalLoginForm);
