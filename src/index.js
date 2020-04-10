381ab8cf031bab43aa43e1a06c9e513e5be3d1a0
Panos61 <panagiwtis.orovas@gmail.com>
2020-04-10 21:49:53 +0300

    redux update

--- src/index.js
+++ src/index.js
@@ -23,11 +23,6 @@ import { LOGIN_SUCCESS } from './actions/authTypes';
 import Security from './Profile/Security/security';
 
 // Stay authenticated after refreshing page
-// const token = localStorage.getItem('token');
-// if (token) {
-//   store.dispatch({ type: USER_LOADED });
-// }
-
 if (localStorage.token) {
   localStorage.getItem('token');
   let user =
