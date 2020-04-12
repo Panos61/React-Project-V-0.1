8dd0539ed784a9d69844f9adff2ef304f081ae09
Panos61 <panagiwtis.orovas@gmail.com>
2020-04-12 15:49:17 +0300

    Setting Create-Event

--- src/CreateEvent/form.js
+++ src/CreateEvent/form.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import { Steps, Button, message } from 'antd';
+import { Col, Row } from 'react-flexbox-grid';
 import './form.css';
 
 const { Step } = Steps;
 
 const steps = [
   {
-    title: 'First',
+    title: 'Γενικά',
     content: 'First-Content',
   },
   {
-    title: 'Second',
+    title: 'Περιεχόμενο',
     content: 'Second-content',
   },
   {
-    title: 'Last',
+    title: 'Ημερομηνία',
     content: 'Last-content',
+  },
+  {
+    title: 'Πολυμέσα',
+    content: 'digital content',
   },
 ];
 
@@ -42,33 +47,44 @@ class FormCE extends Component {
 
     return (
       <div>
-        <Steps current={current}>
-          {steps.map((item) => (
-            <Step key={item.title} title={item.title} />
-          ))}
-        </Steps>
+        <Row style={{ marginTop: '20vh' }}>
+          <Col xs={12}>
+            <Row center="xs">
+              <Col xs={6}>
+                <Steps current={current}>
+                  {steps.map((item) => (
+                    <Step key={item.title} title={item.title} />
+                  ))}
+                </Steps>
 
-        <div className="steps-content">{steps[current].content}</div>
-        <div className="steps-action">
-          {current < steps.length - 1 && (
-            <Button type="primary" onClick={() => this.next()}>
-              Next
-            </Button>
-          )}
-          {current === steps.length - 1 && (
-            <Button
-              type="primary"
-              onClick={() => message.success('Processing complete!')}
-            >
-              Done
-            </Button>
-          )}
-          {current > 0 && (
-            <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
-              Previous
-            </Button>
-          )}
-        </div>
+                <div className="steps-content">{steps[current].content}</div>
+                <div className="steps-action">
+                  {current < steps.length - 1 && (
+                    <Button type="primary" onClick={() => this.next()}>
+                      Next
+                    </Button>
+                  )}
+                  {current === steps.length - 1 && (
+                    <Button
+                      type="primary"
+                      onClick={() => message.success('Processing complete!')}
+                    >
+                      Done
+                    </Button>
+                  )}
+                  {current > 0 && (
+                    <Button
+                      style={{ marginLeft: 8 }}
+                      onClick={() => this.prev()}
+                    >
+                      Previous
+                    </Button>
+                  )}
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
