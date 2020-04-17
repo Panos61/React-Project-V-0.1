2e0bca5c374604e599eb33cb3948099fce430808
Panos61 <panagiwtis.orovas@gmail.com>
2020-04-17 14:36:26 +0300

    test

--- src/CreateEvent/form.js
+++ src/CreateEvent/form.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
 import { Steps, Button, message } from 'antd';
 import { Col, Row } from 'react-flexbox-grid';
-import './form.css';
+//import './form.css';
+import StepOne from './forms/step1';
+import StepThree from './forms/step3';
 
 const { Step } = Steps;
-
-const steps = [
-  {
-    title: 'Γενικά',
-    content: 'First-Content',
-  },
-  {
-    title: 'Περιεχόμενο',
-    content: 'Second-content',
-  },
-  {
-    title: 'Ημερομηνία',
-    content: 'Last-content',
-  },
-  {
-    title: 'Πολυμέσα',
-    content: 'digital content',
-  },
-];
 
 class FormCE extends Component {
   constructor(props) {
@@ -45,46 +28,66 @@ class FormCE extends Component {
   render() {
     const { current } = this.state;
 
+    const steps = [
+      {
+        title: 'Βήμα 1ο',
+        content: <StepOne />,
+      },
+      {
+        title: 'Βήμα 2ο',
+        content: <StepOne />,
+      },
+      {
+        title: 'Βήμα 3ο',
+        content: <StepThree />,
+      },
+      {
+        title: 'Βήμα 4ο',
+        content: <StepOne />,
+      },
+      {
+        title: 'Βήμα 5ο',
+        content: <StepThree />,
+      },
+    ];
     return (
       <div>
-        <Row style={{ marginTop: '20vh' }}>
-          <Col xs={12}>
-            <Row center="xs">
-              <Col xs={6}>
-                <Steps current={current}>
-                  {steps.map((item) => (
-                    <Step key={item.title} title={item.title} />
-                  ))}
-                </Steps>
-
-                <div className="steps-content">{steps[current].content}</div>
-                <div className="steps-action">
-                  {current < steps.length - 1 && (
-                    <Button type="primary" onClick={() => this.next()}>
-                      Next
-                    </Button>
-                  )}
-                  {current === steps.length - 1 && (
-                    <Button
-                      type="primary"
-                      onClick={() => message.success('Processing complete!')}
-                    >
-                      Done
-                    </Button>
-                  )}
-                  {current > 0 && (
-                    <Button
-                      style={{ marginLeft: 8 }}
-                      onClick={() => this.prev()}
-                    >
-                      Previous
-                    </Button>
-                  )}
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+        <Steps
+          current={current}
+          size="small"
+          type="navigation"
+          style={{ marginBottom: '5vh' }}
+        >
+          {steps.map((item) => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button
+              type="primary"
+              onClick={() => this.next()}
+              style={{ margin: 8 }}
+            >
+              Επόμενο
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button type="danger" onClick={() => message.success('Επιτυχία')}>
+              Ολοκλήρωση
+            </Button>
+          )}
+          {current > 0 && (
+            <Button
+              style={{ margin: 8 }}
+              onClick={() => this.prev()}
+              htmlType="submit"
+            >
+              Πίσω
+            </Button>
+          )}
+        </div>
       </div>
     );
   }
