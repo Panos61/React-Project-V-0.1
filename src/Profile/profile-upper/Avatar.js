6b42cc346d35c903a6e10d6d9f2d1bb1d3f639ce
Panos61 <panagiwtis.orovas@gmail.com>
2020-05-02 02:27:05 +0300

    Deleted - Renamed files

--- src/Profile/profile-upper/Avatar.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import { UserOutlined } from '@ant-design/icons';
-import { Avatar } from 'antd';
-
-<Avatar size={144} icon={<UserOutlined />} />;
-
-export default Avatar;
