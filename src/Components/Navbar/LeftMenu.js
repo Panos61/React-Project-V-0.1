04786e5aaae9cc174ebbdf5b67780b7d57c81a69
Panos61 <panagiwtis.orovas@gmail.com>
2020-05-24 20:25:01 +0300

    edit

--- src/Components/Navbar/LeftMenu.js
+++ src/Components/Navbar/LeftMenu.js
@@ -25,29 +25,29 @@ class LeftMenu extends Component {
           </Link>
         </Menu.Item>
         <Menu.Item>
-          <Link to="/" style={{ color: '#fa541c' }}>
+          <Link to="/cinema" style={{ color: '#fa541c' }}>
             Σινεμά
           </Link>
         </Menu.Item>
         <SubMenu title={<span style={{ color: 'grey' }}>Λοιπά</span>}>
           <MenuItemGroup title="Κατηγορίες">
             <Menu.Item key="setting:2">
-              <Link to="/" style={{ color: '#faad14' }}>
+              <Link to="/arts" style={{ color: '#faad14' }}>
                 Τέχνες
               </Link>
             </Menu.Item>
             <Menu.Item key="setting:3">
-              <Link to="/" style={{ color: 'brown' }}>
+              <Link to="/dance" style={{ color: 'brown' }}>
                 Χορός
               </Link>
             </Menu.Item>
             <Menu.Item key="setting:4">
-              <Link to="/" style={{ color: '#096dd9' }}>
+              <Link to="/kids" style={{ color: '#096dd9' }}>
                 Παιδικά
               </Link>
             </Menu.Item>
             <Menu.Item key="setting:5">
-              <Link to="/" style={{ color: 'yellowgreen' }}>
+              <Link to="/social_events" style={{ color: 'yellowgreen' }}>
                 Social_Events
               </Link>
             </Menu.Item>
