63ab1163f4d2474557840483ad93058fb80151e9
Panos61 <panagiwtis.orovas@gmail.com>
2020-05-27 01:24:54 +0300

    Event Data Display dev

---
+++ src/Events/Event.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Events.css';
+import { useSelector } from 'react-redux';
+import { Card } from 'antd';
+import { CarryOutOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+
+const Event = ({ event }) => {
+  const currentState = useSelector((state) => state);
+  const authID = currentState.Auth.currentUser
+    ? currentState.Auth.currentUser.id
+    : '';
+
+  return (
+    <Card
+      className="event-card"
+      key={event.id}
+      title={event.title}
+      style={{ marginTop: '20px' }}
+      size="small"
+      hoverable={true}
+      cover={
+        <img src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />
+      }
+      actions={[<CarryOutOutlined key="interested" />]}
+    >
+      <Meta description={event.description} />
+    </Card>
+  );
+};
+
+export default Event;
--- src/Events/Layout/DataDisplay.js
+++ src/Events/Layout/DataDisplay.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Affix, Button } from 'antd';
+import React, { useEffect } from 'react';
+import Moment from 'react-moment';
+import 'moment/locale/el';
+import { useSelector, useDispatch } from 'react-redux';
+import { Affix, Button, Divider, BackTop, Layout } from 'antd';
 import './DataLayout.css';
+import { fetchEvent } from '../../store/modules/events/actions/eventAction';
 
-const DataDisplay = ({ event }) => {
+import { HeartFilled } from '@ant-design/icons';
+const { Footer } = Layout;
+
+const DataDisplay = (props) => {
+  const eventID = props.match.params.id;
+
+  const dispatch = useDispatch();
+  const singleEvent = (id) => dispatch(fetchEvent(id));
+
   const currentState = useSelector((state) => state);
+
+  const event = currentState.Event.event;
+
   const authID = currentState.Auth.currentUser
     ? currentState.Auth.currentUser.id
     : '';
 
+  useEffect(() => {
+    singleEvent(eventID);
+  }, []);
+
   return (
-    <>
+    <div>
       <section id="event-data-layout">
-        <Affix offsetTop={100}>
+        {/* <Affix offsetTop={100}>
           <Button
             size="large"
             onClick={() => window.history.back()}
@@ -20,11 +38,51 @@ const DataDisplay = ({ event }) => {
           >
             Πίσω
           </Button>
-        </Affix>
+        </Affix> */}
 
-        <span>{event.title}</span>
+        <div className="event-container-parent">
+          <div className="event-container">
+            <div className="event-title">
+              {event.title}{' '}
+              <span style={{ fontSize: 'small' }}>{event.id}</span>
+              <span style={{ float: 'right', fontSize: 'initial' }}>
+                <Moment fromNow locale="el">
+                  {event.created_at}
+                </Moment>{' '}
+                <br />
+                <span style={{ fontSize: 'small' }}>{event.category}</span>
+              </span>
+            </div>
+
+            <Divider />
+            <div className="half-one">
+              <h4>Δημιουργός:</h4>
+              <div className="event-creator" style={{ marginBottom: '15px' }}>
+                {event.creator ? event.creator.username : ''}
+              </div>
+              <h4>Περιγραφή:</h4>
+              <div className="event-description">{event.description}</div>
+            </div>
+            {/* <div className="half-two">
+              <p>content, maps..</p>
+            </div> */}
+          </div>
+          {/* <Divider type="vertical" /> */}
+        </div>
+        <BackTop />
       </section>
-    </>
+
+      <footer>
+        <Footer
+          style={{
+            textAlign: 'center',
+            backgroundColor: 'unset',
+          }}
+        >
+          EventPark ©2020 Created with <HeartFilled /> by us!
+        </Footer>
+      </footer>
+    </div>
   );
 };
 
