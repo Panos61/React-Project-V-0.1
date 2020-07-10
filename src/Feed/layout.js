314ab6556f9832609a49dfcec71d698b5325c569
Panos61 <panagiwtis.orovas@gmail.com>
2020-07-10 18:37:50 +0300

    Feed Grid Fix

--- src/Feed/layout.js
+++ src/Feed/layout.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  margin-left: auto;
   display: grid;
-  grid-template-columns: 15% auto;
+  grid-template-columns: 15% auto 23%;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media screen and (max-width: 1221px) {
+    grid-template-columns: 1f;
+    margin-left: auto;
+  }
+
   @media screen and (max-width: 1160px) {
+    display: grid;
+    grid-template-columns: 10% auto 23%;
+    margin-left: auto;
+  }
+
+  @media screen and (max-width: 1101px) {
+    display: grid;
     grid-template-columns: 10% auto;
     margin-left: auto;
   }
+
   @media screen and (max-width: 636px) {
     margin-left: 0;
     grid-template-columns: 1fr;
